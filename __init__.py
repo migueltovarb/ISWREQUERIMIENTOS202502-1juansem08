6b0928cf232a6934d90ebfddce9adc9ba2d878ee
juansem08 <juan.mayorgaj@campusucc.edu.co>
2025-11-22 15:32:37 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Simulador de Ecuaciones Diferenciales
-
-Este paquete proporciona herramientas para simular diferentes modelos de ecuaciones diferenciales
-con una interfaz gráfica amigable.
-"""
-
-__version__ = '0.1.0'
